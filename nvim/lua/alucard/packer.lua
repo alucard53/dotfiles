50f5ce1d0e4339ee06d7b34d60b46ac2ec320a6a
sarthak <75154468+alucard53@users.noreply.github.com>
2024-03-31 00:45:32 +0530

    Update packer.lua

--- nvim/lua/alucard/packer.lua
+++ nvim/lua/alucard/packer.lua
@@ -5,12 +5,17 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use({
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        after = "nvim-treesitter",
+        requires = "nvim-treesitter/nvim-treesitter",
+    })
 
     use {
         "windwp/nvim-autopairs",
@@ -27,27 +32,28 @@ return require('packer').startup(function(use)
             -- LSP Support
             { 'neovim/nvim-lspconfig' }, -- Required
             {                            -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
 
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-        }
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },     -- Required
+        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+        { 'L3MON4D3/LuaSnip' },     -- Required
     }
+}
 
-    use 'martinsione/darkplus.nvim'
+use 'martinsione/darkplus.nvim'
+use 'ntk148v/komau.vim'
 
-    use 'nvim-tree/nvim-web-devicons'
+use 'nvim-tree/nvim-web-devicons'
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+}
 end)
